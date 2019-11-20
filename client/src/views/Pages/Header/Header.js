3de66d412f03e0939c967e22bed4df5294c52ca3
Onoja <onojamatthew59@gmail.com>
2019-11-20 15:54:21 +0100

    modified login and registration pages

--- client/src/views/Pages/Header/Header.js
+++ client/src/views/Pages/Header/Header.js
@@ -14,6 +14,7 @@ import {
   Container,
   // NavbarText
 } from 'reactstrap';
+
 import logo from "../../../assets/img/brand/ojirehprime_logo.png";
 
 const Header = ( props ) => {
@@ -21,37 +22,72 @@ const Header = ( props ) => {
 
   const toggle = () => setIsOpen( !isOpen );
 
+  const styles = {
+    nav: {
+      color: "#fff"
+    }
+  }
+
   return (
     <div className="custom-header">
       <Navbar color="light" expand="md">
         <Container>
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarBrand href="/">
+          <img src={logo} alt="" />
+        </NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
             <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                <NavLink
+                  href="/components/"
+                  style={styles.nav}
+                >Home</NavLink>
             </NavItem>
             <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
+                <DropdownToggle nav caret style={styles.nav}>
+                About Us
               </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    What We Do
+                  </DropdownItem>
+                  <DropdownItem>
+                    Our Vision, Mission, Core Values
+                  </DropdownItem>
+                  <DropdownItem>
+                    Roadmap
+                  </DropdownItem>
+                  <DropdownItem>
+                    Our Team
+                  </DropdownItem>
+                </DropdownMenu>
+                </UncontrolledDropdown>
+              <NavItem>
+                <NavLink
+                  href="/components/"
+                  style={styles.nav}
+                >FAQs</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  href="/components/"
+                  style={styles.nav}
+                >Blog</NavLink>
+                
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  href="/components/"
+                  style={styles.nav}
+                >Careers</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  href="/components/"
+                  style={styles.nav}
+                >Online Store</NavLink>
+              </NavItem>
           </Nav>
           </Collapse>
         </Container>
