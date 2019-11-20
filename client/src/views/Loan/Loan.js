b0d30426534b0cf93953ed5a768daeb20ba04393
Onoja <onojamatthew59@gmail.com>
2019-11-20 16:37:38 +0100

    implemented otp for loan

--- client/src/views/Loan/Loan.js
+++ client/src/views/Loan/Loan.js
@@ -14,14 +14,39 @@ import {
   Form,
 } from "reactstrap";
 import { isAuthenticated } from "../../helper/authenticate";
-import { verifyNumber, verifyOtp } from "../../store/actions/action_loan";
+import { verifyOtp } from "../../store/actions/action_loan";
+import { sendOTP } from "../../store/actions/action_login";
 
 class Loan extends Component {
-  // state = {
-  //   otp: "",
-  //   phone: ""
-  // }
+  state = {
+    otp: "",
+    phone: ""
+  }
+
+  onChange = ( e, name ) => {
+    let fields = this.state;
+    fields[ name ] = e.target.value;
+    this.setState( { fields } );
+    console.log( this.state );
+  }
+
+  handleVerification = async (e, name) => {
+    e.preventDefault();
+    const { phone, otp } = this.state;
+    const { sendOTP, verifyOtp } = this.props;
+    
+    try {
+      if ( name === "phone" ) {
+        await sendOTP( phone );
+      } else {
+        await verifyOtp( otp );
+      }
+    } catch(err) {}
+  }
+
   render() {
+    const { loan } = this.props;
+    const { phone } = this.state;
     const user = isAuthenticated().user && isAuthenticated().user
     return (
       <div>
@@ -31,9 +56,7 @@ class Loan extends Component {
               <Col xs="12" xl="5">
                 <h3>Welcome {user.name}</h3>
                 <p className="text-muted">Please enter your number to see your loan offer</p>
-                <Form onSubmit={"onLogin"}>
-                  {/* {errMsg.length > 0 ? <p style={{ color: "#ff0000" }}>{errMsg}</p> : null}
-                  {login.error && login.error.length > 0 ? <p style={{ color: "#ff0000" }}>{login.error}</p> : null} */}
+                <Form onSubmit={(e) => this.handleVerification(e, "phone")}>
                   <InputGroup className="mb-3">
                     <InputGroupAddon addonType="prepend">
                       <InputGroupText>
@@ -43,16 +66,16 @@ class Loan extends Component {
                     <Input
                       type="tel"
                       placeholder="0xxx xxx xxxx"
-                      // value={otp}
-                      // onChange={( e ) => handleChange( e, "otp" )}
+                      value={phone}
+                      onChange={( e ) => this.onChange( e, "phone" )}
                     />
                   </InputGroup>
 
                   <Row>
                     <Col xs="12">
-                      {/* {login.loading === true ? <Spinner color="primary" /> : ( */}
+                      {loan.loading === true ? <Spinner color="primary" /> : (
                         <Button color="success" className="px-4">Send</Button>
-                      {/* )} */}
+                      )}
                     </Col>
                   </Row>
                 </Form>
@@ -66,12 +89,15 @@ class Loan extends Component {
 }
 
 const mapStateToProps = ( state ) => {
-  return {}
+  return {
+    loan: state.loan,
+    login: state.login
+  }
 }
 
 const mapDispatchToProps = ( dispatch ) => {
   const dispatchProps = {
-    verifyNumber: ( data ) => dispatch( verifyNumber( data ) ),
+    sendOTP: ( data ) => dispatch( sendOTP(data) ),
     verifyOtp: (data) => dispatch(verifyOtp(data))
   }
 
