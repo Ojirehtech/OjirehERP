d8a30f3492c130577b65ed61bdde045515480f66
Onoja <onojamatthew59@gmail.com>
2019-11-22 07:13:32 +0100

    added new updates

--- client/src/views/Loan/Menu/Container.js
+++ client/src/views/Loan/Menu/Container.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
+import Payloan from "./Payloan";
+import ActionForm from "./ActionForm";
+import OfferPage from "./OfferPage";
 
 class Container extends Component{
+  state = {
+    action: ""
+  }
+
+  handleChange = ( e ) => {
+    this.setState( {
+      action: e.target.value
+    } );
+    console.log(e.target.value)
+  }
+
+  renderView = () => {
+    const { action} = this.state;
+    switch (action) {
+      case "pay":
+        return <Payloan />
+      case "offer":
+        return <OfferPage />
+      default:
+        return <ActionForm handleChange={this.handleChange} />;
+    }
+  }
   render() {
     return (
       <div>
-        
+       {this.renderView()}
       </div>
-    )
+    );
   }
 }
 
---
+++ client/src/views/Loan/Menu/OfferPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class OfferPage extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Offer page</h3>
+      </div>
+    )
+  }
+}
+
+export default OfferPage;
